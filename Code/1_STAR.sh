89a0c9c61115d81674c642aab53cf3d8385d860c
lz711 <68362233+lz711@users.noreply.github.com>
2023-10-10 22:14:00 -0700

    Add files via upload

---
+++ Code/1_STAR.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Function to print help
+print_usage()
+{
+	   echo "Usage: $(basename "$0") [ -t {target_intervals} -f {file set} ]"; 
+	   echo "Where -f override file(s) to be run in pipeline, default = '*.fastq.gz'";
+	   return
+}
+
+# set default parameters
+fset=""; 
+default_fset='*.fastq.gz'
+
+# Parse command line options
+OPTIND=1
+while getopts "f:h" OPT
+do
+  case "$OPT" in
+    f) fset="$OPTARG";;
+    h) print_usage; exit 1;;
+   \?) print_usage; exit 1;;
+    :) echo "Option -$OPTARG requires an argument."; print_usage; exit 1;;
+  esac
+done
+
+if [ "$fset" == "" ]; then
+  fset=$default_fset
+fi
+
+# Turn on extended globbing (allowing regex in file matching patterns)
+shopt -s extglob
+
+# STAR-Fusion requires STAR (v2.6.0+) and samtools to be in PATH
+export PATH=$PATH:/mnt/ix1/Resources/tools/STAR/v2.7.9a/bin/Linux_x86_64:/mnt/ix1/Resources/tools/samtools-1.15
+
+# Alignment and transcript counting
+MAX_JAVA_MEM=2g ${BPIPE_EXE} run -n $MAX_THREADS -l mem=$GB_MEM -rf "${LOG_REPORT}.html" \
+                                 ${PIPES_DIR}/rnabulk_starJ.pipe ${fset}
---
+++ Code/2_htseq_counts_tpm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ct_dir='.'
+>all.htseq.counts.txt
+
+i=0
+for fn in ${ct_dir}/P*.Aligned.htseq.counts.txt 
+do
+  ((i+=1))
+  basefn=$(basename $fn)
+  sample=$(echo $basefn | awk -F. '{ print $1 }')
+  echo "Processing sample $sample"
+  if [[ $i -eq 1 ]]; then sed "1i Gene\t$sample" $fn | cut -f1 >all.htseq.counts.txt; fi;
+  sed "1i Gene\t$sample" $fn | cut -f2 | paste all.htseq.counts.txt - >tmp.htseq.counts.txt
+  mv tmp.htseq.counts.txt all.htseq.counts.txt
+done
+
+
+###TPMs
+x <- counts / gene_length[, "Length"]
+tpm_mat <- t( t(x) * 1e6 / colSums(x) )  
+
+matrix<- log2(tpm_mat+1)
